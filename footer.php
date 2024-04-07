12859cd9907161d771e0ce551be477df1fe03a26
1zu0 <67436621+1zu0@users.noreply.github.com>
2024-04-07 15:56:16 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+	 <div class="container pageFooter">
+			<div id="contentCurve"></div>
+			<p class="text-muted disclaimer text-center ng-binding" style="font-size: 11px">If you register a new account, you agree to TikTok's <a href="https://www.tiktok.com/legal/terms-of-use" target="_blank">Terms & Conditions</a> and <a href="https://www.tiktok.com/legal/privacy-policy" target="_blank">Privacy Policy</a>.</p>
+	 </div>
+</footer>
+</body>
+</html>
